f9b7638f18b7d3b254f4170338b5dbad27476a4d
cortesmc <carlosandres149@gmail.com>
2024-03-20 11:43:28 +0100

    [MODIF]Class message.java

--- src/NetworkApplication/Message.java
+++ src/NetworkApplication/Message.java
@@ -2,22 +2,33 @@ package NetworkApplication;
 
 public class Message {
 
-    public final static int HELLOWORLD = 0;
-
+    private int scrIndex;
+    private int srcID;
+    private int reqIndex;
+    private int reqID;
     private int type;
-    private String content;
+    private int newConnectionId;
 
-    Message(int type, String content) {
-	this.type = type;
-	this.content = content;
+    private int newConnectionIndex;
+    Message(int scrIndex, int srcID, int reqIndex, int reqID, int type) {
+	this.scrIndex = scrIndex;
+    this.srcID = srcID;
+    this.reqIndex = reqIndex;
+    this.reqID = reqID;
+    this.type = type;
     }
 
-    public String getContent() {
-	return this.content;
-    }
+    public void setSrcID(int srcID) {this.srcID = srcID;}
+    public void setReqIndex(int reqIndex) {this.reqIndex = reqIndex;}
+    public void setReqID(int reqID) {this.reqID = reqID;}
+    public void setType(int type) {this.type = type;}
+    public void setNewConnectionId(int newConnectionId) {this.newConnectionId = newConnectionId;}
+    public void setNewConnectionIndex(int newConnectionIndex) {this.newConnectionIndex = newConnectionIndex;}
 
-    public int getType() {
-	return this.type;
-    }
-    
+    public int getSrcID() {return srcID;}
+    public int getReqIndex() {return reqIndex;}
+    public int getReqID() {return reqID;}
+    public int getType() {return type;}
+    public int getNewConnectionId() {return newConnectionId;}
+    public int getNewConnectionIndex() {return newConnectionIndex;}
 }
