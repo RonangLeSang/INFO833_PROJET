11c455c9d42ccb39e2af1502895ccdce1964ca7d
cortesmc <carlosandres149@gmail.com>
2024-03-21 19:36:35 +0100

    [MODIF]REFACTOR2

--- src/NetworkApplication/ApplicationLayer.java
+++ src/NetworkApplication/ApplicationLayer.java
@@ -8,152 +8,22 @@ import java.util.HashMap;
 import java.util.Objects;
 
 public class ApplicationLayer implements EDProtocol {
-    
-    //identifiant de la couche transport
+
+    // Identifier of the transport layer
     private int transportPid;
-
-    //objet couche transport
+    // Transport layer object
     private TransportLayer transport;
-
-    //identifiant de la couche courante (la couche applicative)
+    // Identifier of the current layer (the application layer)
     private int mypid;
-
-    //le numero de noeud
+    // Node number
     private int nodeId;
-
-    //prefixe de la couche (nom de la variable de protocole du fichier de config)
+    // Prefix of the layer (protocol variable name from the configuration file)
     private String prefix;
     private int index;
-
-    //key : nodeID
-    //value : nodeIndex
+    // HashMap<key, value> = HashMap<nodeID, nodeIndex>
     private HashMap<Integer, Integer> rightNeighbour = null;
-
     private HashMap<Integer, Integer> leftNeighbour = null;
 
-    public HashMap<Integer, Integer> getRightNeighbour() {
-        return rightNeighbour;
-    }
-
-    public HashMap<Integer, Integer> getLeftNeighbour() {
-        return leftNeighbour;
-    }
-
-    public ApplicationLayer(String prefix) {
-        this.prefix = prefix;
-        //initialisation des identifiants a partir du fichier de configuration
-        this.transportPid = Configuration.getPid(prefix + ".transport");
-        this.mypid = Configuration.getPid(prefix + ".myself");
-        this.transport = null;
-    }
-
-    public TransportLayer getTransport() {
-        return transport;
-    }
-
-    public void setIndex(int index) {
-        this.index = index;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    //methode appelee lorsqu'un message est recu par le protocole ApplicationLayer du noeud
-
-    // type du message :
-    // 0 -> requête pour trouver sa place
-    // 1 -> requête d'insertion de noeuds
-    // 2 -> une fois le noeud placé, oon informe les autres noeuds de son arrivé
-    // 3 -> setBothNeighbours
-    // 4 -> set right neighbour
-    // 5 -> set left neighbour
-    // 6 -> leave
-    public void processEvent( Node node, int pid, Object receivedMessage) {
-
-        String reqIndex = "reqIndex";
-        String reqID = "reqID";
-        String srcID = "srcID";
-        String srcIndex = "srcIndex";
-
-        HashMap message = (HashMap<String, Integer>)receivedMessage;
-//        System.out.println("recu : " + message);
-        switch ((int)message.get("type")){
-            case 0:
-                System.out.println("case 0");
-                setNeighbours((int)message.get(reqIndex), (int)message.get(reqID));
-                break;
-            case 1:
-                System.out.println("case 1");
-                if (message.get(srcID)==leftNeighbour.keySet().iterator().next()) {
-                    setLeftNeighbourFromInt((int)message.get(reqID), (int)message.get(reqIndex));
-                } else {
-                    setRightNeighbourFromInt((int)message.get(reqID), (int)message.get(reqIndex));
-                }
-                break;
-            case 2:
-                System.out.println("case 2");
-                if ((int)message.get(srcID) < getNodeId()) {
-                    setLeftNeighbourFromInt((int)message.get(srcID), (int)message.get(srcIndex));
-                    setRightNeighbourFromInt((int)message.get("newConnectionID"), (int)message.get("newConnectionIndex"));
-                } else if((int)message.get(srcID) > getNodeId()){
-                    setLeftNeighbourFromInt((int)message.get("newConnectionID"), (int)message.get("newConnectionIndex"));
-                    setRightNeighbourFromInt((int)message.get(srcID), (int)message.get(srcIndex));
-                } else if((int)message.get("newConnectionID") < getNodeId()){
-                    setRightNeighbourFromInt((int)message.get("newConnectionID"), (int)message.get("newConnectionIndex"));
-                } else {
-                    setLeftNeighbourFromInt((int)message.get("newConnectionID"), (int)message.get("newConnectionIndex"));
-                }
-                break;
-            case 3:
-                System.out.println("case 3");
-                setRightNeighbourFromInt((int)message.get(srcID), (int)message.get(srcIndex));
-                setLeftNeighbourFromInt((int)message.get(srcID), (int)message.get(srcIndex));
-                break;
-            case 4:
-                System.out.println("case 4");
-                setRightNeighbourFromInt((int)message.get("newConnectionID"), (int)message.get("newConnectionIndex"));
-                break;
-            case 5:
-                System.out.println("case 5");
-                setLeftNeighbourFromInt((int)message.get("newConnectionID"), (int)message.get("newConnectionIndex"));
-                break;
-            case 6:
-                System.out.println("case 6");
-                System.out.println("index : " + index + " LEAVING" + nodeId);
-                leave();
-                break;
-            default:
-                System.err.println("message type undefined");
-        }
-        System.out.println(message);
-//        DisplayDHTTask.displayDHT();
-
-    }
-
-    public int getNodeId() {
-        return nodeId;
-    }
-
-    public void setNodeId(int nodeId) {
-        this.nodeId = nodeId;
-    }
-
-    public void setRightNeighbourFromInt( int nodeId, int nodeIndex){
-//        printNeighbours();
-        HashMap<Integer, Integer> newHashMap = new HashMap<>();
-        newHashMap.put(nodeId, nodeIndex);
-        setRightNeighbour(newHashMap);
-        DisplayDHTTask.displayDHT();
-    }
-
-    public void setLeftNeighbourFromInt( int nodeId, int nodeIndex){
-//        printNeighbours();
-        HashMap<Integer, Integer> newHashMap = new HashMap<>();
-        newHashMap.put(nodeId, nodeIndex);
-        setLeftNeighbour(newHashMap);
-        DisplayDHTTask.displayDHT();
-    }
 
     public void setRightNeighbour(HashMap<Integer, Integer> rightNeighbour) {
         this.rightNeighbour = rightNeighbour;
@@ -163,17 +33,95 @@ public class ApplicationLayer implements EDProtocol {
         this.leftNeighbour = leftNeighbour;
     }
 
-    private boolean isFirstNode(){
-//        System.out.println(" | " + leftNeighbour.keySet().iterator().next() + " | " + rightNeighbour.keySet().iterator().next() + " | " + getNodeId());
-        return Objects.equals(leftNeighbour.keySet().iterator().next(), rightNeighbour.keySet().iterator().next()) && rightNeighbour.keySet().iterator().next() == getNodeId();
+    public void setNodeId(int nodeId) {
+        this.nodeId = nodeId;
     }
 
-    private boolean isDHTBeginning(){
-        return leftNeighbour.keySet().iterator().next() > nodeId;
+    public void setIndex(int index) {
+        this.index = index;
     }
 
-    private boolean isDHTEnding(){
-        return rightNeighbour.keySet().iterator().next() < nodeId;
+    public void setTransportLayer(int index) {
+        this.transport = (TransportLayer) Network.get(index).getProtocol(this.transportPid);
+    }
+
+    public TransportLayer getTransport() {
+        return transport;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public int getNodeId() {
+        return nodeId;
+    }
+
+    public ApplicationLayer(String prefix) {
+        this.prefix = prefix;
+        // Initialization of identifiers from the configuration file
+        this.transportPid = Configuration.getPid(prefix + ".transport");
+        this.mypid = Configuration.getPid(prefix + ".myself");
+        this.transport = null;
+    }
+
+    public void processEvent( Node node, int pid, Object receivedMessage) {
+
+        String reqIndex = "reqIndex";
+        String reqID = "reqID";
+        String srcID = "srcID";
+        String srcIndex = "srcIndex";
+
+        HashMap message = (HashMap<String, Integer>) receivedMessage;
+        switch ((int) message.get("type")) {
+            case 0:     // -> Request to find its place
+                System.out.println("case 0");
+                setNeighbours((int) message.get(reqIndex), (int) message.get(reqID));
+                break;
+            case 1:     // -> Node insertion request
+                System.out.println("case 1");
+                if (message.get(srcID) == leftNeighbour.keySet().iterator().next()) {
+                    setLeftNeighbourFromInt((int) message.get(reqID), (int) message.get(reqIndex));
+                } else {
+                    setRightNeighbourFromInt((int) message.get(reqID), (int) message.get(reqIndex));
+                }
+                break;
+            case 2:    // -> Once the node is placed, inform other nodes of its arrival
+                System.out.println("case 2");
+                if ((int) message.get(srcID) < getNodeId()) {
+                    setLeftNeighbourFromInt((int) message.get(srcID), (int) message.get(srcIndex));
+                    setRightNeighbourFromInt((int) message.get("newConnectionID"), (int) message.get("newConnectionIndex"));
+                } else if ((int) message.get(srcID) > getNodeId()) {
+                    setLeftNeighbourFromInt((int) message.get("newConnectionID"), (int) message.get("newConnectionIndex"));
+                    setRightNeighbourFromInt((int) message.get(srcID), (int) message.get(srcIndex));
+                } else if ((int) message.get("newConnectionID") < getNodeId()) {
+                    setRightNeighbourFromInt((int) message.get("newConnectionID"), (int) message.get("newConnectionIndex"));
+                } else {
+                    setLeftNeighbourFromInt((int) message.get("newConnectionID"), (int) message.get("newConnectionIndex"));
+                }
+                break;
+            case 3:     // -> Set both neighbours
+                System.out.println("case 3");
+                setRightNeighbourFromInt((int) message.get(srcID), (int) message.get(srcIndex));
+                setLeftNeighbourFromInt((int) message.get(srcID), (int) message.get(srcIndex));
+                break;
+            case 4:     // -> Set right neighbour
+                System.out.println("case 4");
+                setRightNeighbourFromInt((int) message.get("newConnectionID"), (int) message.get("newConnectionIndex"));
+                break;
+            case 5:     // -> Set left neighbour
+                System.out.println("case 5");
+                setLeftNeighbourFromInt((int) message.get("newConnectionID"), (int) message.get("newConnectionIndex"));
+                break;
+            case 6:     // -> Leave
+                System.out.println("case 6");
+                System.out.println("index : " + index + " LEAVING" + nodeId);
+                leave();
+                break;
+            default:
+                System.err.println("message type undefined");
+        }
+        System.out.println(message);
     }
 
     public void setNeighbours(int reqIndex, int reqID) {
@@ -184,15 +132,13 @@ public class ApplicationLayer implements EDProtocol {
         message.put("reqID", reqID);
         Node nodeSrc = Network.get(index);
 
-        // fait
-        if (isFirstNode()){ // cas ou on a une seule node
+        if (isFirstNode()) { // case where there is only one node
             setRightNeighbourFromInt(reqID, reqIndex);
             setLeftNeighbourFromInt(reqID, reqIndex);
             message.put("type", 3);
             transport.send(nodeSrc, Network.get(reqIndex), new HashMap<>(message), 0);
-
-        } else if (reqID < getNodeId()) { // si on est pas le premier noeud, et que il faut envoyer la requête à gauche
-            if (isDHTBeginning() || reqID > leftNeighbour.keySet().iterator().next()) { // cas ou on est au début de la DHT -> on insère le noeud directement à gauche
+        } else if (reqID < getNodeId()) { // if we are not the first node, and the request needs to be sent left
+            if (isDHTBeginning() || reqID > leftNeighbour.keySet().iterator().next()) { // case where we are at the beginning of the DHT -> insert the node directly to the left
 
                 message.put("type", 1);
                 transport.send(nodeSrc, Network.get(leftNeighbour.values().iterator().next()), new HashMap<>(message), 0);
@@ -200,14 +146,14 @@ public class ApplicationLayer implements EDProtocol {
                 message.put("type", 2);
                 message.put("newConnectionIndex", leftNeighbour.values().iterator().next());
                 message.put("newConnectionID", leftNeighbour.keySet().iterator().next());
-                transport.send(nodeSrc, Network.get(reqIndex), new HashMap<>(message), 0); // on envoi au noeud souhaitant s'insérer l'ordre de le faire
+                transport.send(nodeSrc, Network.get(reqIndex), new HashMap<>(message), 0); // send the order to the node wishing to insert itself
                 setLeftNeighbourFromInt(reqID, reqIndex);
             } else {
                 message.put("type", 0);
                 transport.send(nodeSrc, Network.get(leftNeighbour.values().iterator().next()), new HashMap<>(message), 0);
             }
-        } else { // si on est pas le premier noeud, et que il faut envoyer la requête à droite
-            if (isDHTEnding() || reqID < rightNeighbour.keySet().iterator().next()) { // cas ou on est au début de la DHT -> on insère le noeud directement à gauche
+        } else { // if we are not the first node, and the request needs to be sent right
+            if (isDHTEnding() || reqID < rightNeighbour.keySet().iterator().next()) { // case where we are at the end of the DHT -> insert the node directly to the right
 
                 message.put("type", 1);
                 transport.send(nodeSrc, Network.get(rightNeighbour.values().iterator().next()), new HashMap<>(message), 0);
@@ -215,7 +161,7 @@ public class ApplicationLayer implements EDProtocol {
                 message.put("type", 2);
                 message.put("newConnectionIndex", rightNeighbour.values().iterator().next());
                 message.put("newConnectionID", rightNeighbour.keySet().iterator().next());
-                transport.send(nodeSrc, Network.get(reqIndex), new HashMap<>(message), 0); // on envoi au noeud souhaitant s'insérer l'ordre de le faire
+                transport.send(nodeSrc, Network.get(reqIndex), new HashMap<>(message), 0); // send the order to the node wishing to insert itself
                 setRightNeighbourFromInt(reqID, reqIndex);
             } else {
                 message.put("type", 0);
@@ -225,8 +171,8 @@ public class ApplicationLayer implements EDProtocol {
         message.put("type", 0);
     }
 
-    public void leave(){
-        if((rightNeighbour != null && leftNeighbour != null) && !isFirstNode()){
+    public void leave() {
+        if ((rightNeighbour != null && leftNeighbour != null) && !isFirstNode()) {
             Node nodeSrc = Network.get(index);
             HashMap<String, Integer> message = new HashMap<>();
             message.put("srcIndex", index);
@@ -235,48 +181,55 @@ public class ApplicationLayer implements EDProtocol {
             message.put("newConnectionIndex", rightNeighbour.values().iterator().next());
             message.put("newConnectionID", rightNeighbour.keySet().iterator().next());
             message.put("type", 4);
-            transport.send(nodeSrc, Network.get(leftNeighbour.values().iterator().next()), new HashMap<>(message), 0); // on set le voisin droit de notre voisin gauche
+            transport.send(nodeSrc, Network.get(leftNeighbour.values().iterator().next()), new HashMap<>(message), 0); // set the right neighbor of our left neighbor
 
             message.put("newConnectionIndex", leftNeighbour.values().iterator().next());
             message.put("newConnectionID", leftNeighbour.keySet().iterator().next());
             message.put("type", 5);
-            transport.send(nodeSrc, Network.get(rightNeighbour.values().iterator().next()), new HashMap<>(message), 0); // on set le voisin gauche de notre voisin droit
+            transport.send(nodeSrc, Network.get(rightNeighbour.values().iterator().next()), new HashMap<>(message), 0); // set the left neighbor of our right neighbor
 
             setLeftNeighbourFromInt(nodeId, index);
             setRightNeighbourFromInt(nodeId, index);
         }
     }
 
-    //methode necessaire pour la creation du reseau (qui se fait par clonage d'un prototype)
+    public void setRightNeighbourFromInt(int nodeId, int nodeIndex) {
+        HashMap<Integer, Integer> newHashMap = new HashMap<>();
+        newHashMap.put(nodeId, nodeIndex);
+        setRightNeighbour(newHashMap);
+        DisplayDHTTask.displayDHT(); // Assuming DisplayDHTTask.displayDHT() is a method that displays the distributed hash table
+    }
+
+    public void setLeftNeighbourFromInt(int nodeId, int nodeIndex) {
+        HashMap<Integer, Integer> newHashMap = new HashMap<>();
+        newHashMap.put(nodeId, nodeIndex);
+        setLeftNeighbour(newHashMap);
+        DisplayDHTTask.displayDHT(); // Assuming DisplayDHTTask.displayDHT() is a method that displays the distributed hash table
+    }
+
+    // Check if this is the first node in the DHT
+    private boolean isFirstNode() {
+        return Objects.equals(leftNeighbour.keySet().iterator().next(), rightNeighbour.keySet().iterator().next()) && rightNeighbour.keySet().iterator().next() == getNodeId();
+    }
+
+    // Check if this node is at the beginning of the DHT
+    private boolean isDHTBeginning() {
+        return leftNeighbour.keySet().iterator().next() > nodeId;
+    }
+
+    // Check if this node is at the end of the DHT
+    private boolean isDHTEnding() {
+        return rightNeighbour.keySet().iterator().next() < nodeId;
+    }
+
+    // Necessary method for network creation (which is done by cloning a prototype)
     public Object clone() {
         ApplicationLayer dolly = new ApplicationLayer(this.prefix);
         return dolly;
     }
 
-    //liaison entre un objet de la couche applicative et un 
-    //objet de la couche transport situes sur le meme noeud
-    public void setTransportLayer(int index) {
-        this.transport = (TransportLayer) Network.get(index).getProtocol(this.transportPid);
+    // Method to print neighbours (for debugging purposes)
+    public void printNeighbours() {
+        System.out.println("Left neighbor: " + leftNeighbour + " | Node ID: " + nodeId + " | Right neighbor: " + rightNeighbour);
     }
-
-    //affichage a la reception
-    private void receive(Object msg) {
-        System.out.println(this + ": Received " + msg);
-    }
-
-    //retourne le noeud courant
-    private Node getMyNode() {
-        return Network.get(this.nodeId);
-    }
-
-    public int getMypid() {
-        return mypid;
-    }
-
-    public void printNeighbours(){
-        System.out.println("voisin de gauche : " + leftNeighbour + " | node ID : " + nodeId + " | voisin de droite : " + rightNeighbour);
-    }
-
-
-
 }
